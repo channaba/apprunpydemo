23e22e9db5d9f163035a37125418d50cbf7fd508
channaba <83370536+channaba@users.noreply.github.com>
2021-05-19 18:34:01 -0400

    Update pyHello.py

--- pyHello.py
+++ pyHello.py
@@ -5,7 +5,6 @@ PORT = 8080
 name = os.environ['NAME']
 
 if name == None or name.len() == 0:
-  name = "AWS" 
-  
- MESSAGE = "Hello, " + name + "!\n"
-  
+   name = "AWS" 
+    
+MESSAGE = "Hello, " + name + "!\n"
