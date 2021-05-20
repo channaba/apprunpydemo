be17658bf7571edadf68046d50fbdcf1c57499a5
channaba <83370536+channaba@users.noreply.github.com>
2021-05-20 13:06:12 -0400

    Update pyHello.py

--- pyHello.py
+++ pyHello.py
@@ -3,8 +3,18 @@ import os
 
 PORT = 8080
 name = os.environ['NAME']
+if name == None or len(name) == 0:  
+   name = "AWS"
+MESSAGE = "Hello, " + name + "!"
+print("Message: '" + MESSAGE + "'")
 
-if name == None or len(name) == 0:
-   name = "AWS" 
-    
-MESSAGE = "Hello, " + name + "!\n"
+app = Flask(__name__)
+
+@app.route("/")
+def root():  
+print("Handling web request. Returning message.")  
+result = MESSAGE.encode("utf-8")  
+return result
+ 
+if __name__ == "__main__":  
+  app.run(debug=True, host="0.0.0.0", port=PORT)
