071615795e351ecb44af992472949f595a27c8bf
channaba <83370536+channaba@users.noreply.github.com>
2021-05-20 13:21:32 -0400

    Update pyHello.py

--- pyHello.py
+++ pyHello.py
@@ -12,9 +12,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def root():  
-print("Handling web request. Returning message.")  
-result = MESSAGE.encode("utf-8")  
-return result
+  print("Handling web request. Returning message.")  
+  result = MESSAGE.encode("utf-8")  
+  return result
  
 if __name__ == "__main__":  
   app.run(debug=True, host="0.0.0.0", port=PORT)
