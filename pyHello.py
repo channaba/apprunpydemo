96bf32e7497cb398895c481a0d811c1d2ff5b1ae
channaba <83370536+channaba@users.noreply.github.com>
2021-05-26 11:38:58 -0400

    Update pyHello.py

--- pyHello.py
+++ pyHello.py
@@ -1,8 +1,9 @@
 from flask import Flask
 import os
 
-PORT = 8080
-name = os.environ['NAME']
+PORT=8080
+name=os.environ['NAME']
+
 if name == None or len(name) == 0:  
    name = "AWS"
 MESSAGE = "Hello, " + name + "!"
